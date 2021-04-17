4b5e27987a5a70683ca3f4d032f3abe3e0a25316
dxkruse <dxkruse@gmail.com>
2021-04-16 22:44:46 -0500

    video commit

--- Lab 11/Scripts/ICP11.py
+++ Lab 11/Scripts/ICP11.py
@@ -37,34 +37,24 @@ y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 # Create the model
 model = Sequential()
-
 model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Conv2D(64, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Conv2D(128, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dropout(0.2))
-
 model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-
 model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-
 model.add(Dense(num_classes, activation='softmax'))
 # Compile model
 epochs = 15
