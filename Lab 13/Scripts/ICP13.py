e3f451403d48dac5c0ea8d2da502d17c86cec5c1
dxkruse <dxkruse@gmail.com>
2021-04-30 19:35:43 -0500

    ICP13

---
+++ Lab 13/Scripts/ICP13.py
@@ -0,0 +1,82 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist, fashion_mnist
+import numpy as np
+import matplotlib.pyplot as plt
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+
+# Add hidden layer
+hidden = Dense(128, activation='relu')(input_img)
+
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(hidden)
+
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# create model for encoder
+encoder = Model(input_img, encoded)
+encoded_input = Input(shape=(encoding_dim,))
+
+# Set decoder layer variable as last layer from autoencoder
+decoder_layer = autoencoder.layers[-1]
+
+# Create decoder model to map encoded images to decoded images 
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# Fit model and assign to history object
+history = autoencoder.fit(x_train, x_train,
+                epochs=5,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# Make predictions
+encoded_pred = encoder.predict(x_test)
+decoded_pred = decoder.predict(encoded_pred)
+
+
+
+# Display original
+ax = plt.subplot(2, 1, 1)
+plt.imshow(x_test[0].reshape(28, 28))
+
+
+# Display reconstruction
+ax = plt.subplot(2, 1, 2)
+plt.imshow(decoded_pred[0].reshape(28, 28))
+
+# Accuracy Plot
+plot2 = plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='lower right')
+
+# Loss Plot
+plot3 = plt.figure(3)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
